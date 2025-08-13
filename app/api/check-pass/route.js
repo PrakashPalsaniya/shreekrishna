429ad7d14c922f56ada60f7a0ac3a2d076d8c9cb
Prakash Palsaniya <150008749+PrakashPalsaniya@users.noreply.github.com>
2025-08-14 00:51:18 +0530

    Update route.js

--- app/api/check-pass/route.js
+++ app/api/check-pass/route.js
@@ -1,1 +1,14 @@
 
+export async function POST(req) {
+  try {
+    const { password } = await req.json();
+
+    if (password === process.env.ADMIN_PASS) {
+      return new Response(JSON.stringify({ success: true }), { status: 200 });
+    } else {
+      return new Response(JSON.stringify({ success: false }), { status: 401 });
+    }
+  } catch (error) {
+    return new Response(JSON.stringify({ success: false, error: "Server error" }), { status: 500 });
+  }
+}
